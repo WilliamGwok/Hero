9feec085cf8306107b619296e2d23563deef5a45
william0102 <junweigao1@outlook.com>
2022-11-16 00:17:27 +0800

    gimbal improve

--- HeroV1/MDK-ARM/USER/modules/gimbal.c
+++ HeroV1/MDK-ARM/USER/modules/gimbal.c
@@ -5,13 +5,21 @@
 extern motor_t Gim_Yaw;
 extern motor_t Gim_Pitch;
 
-float gimb_y_angle_in_pid_param[7] = {12,3.5,0,0,0,4000,28000};
+gimbal_info_t gimbal_info;
 
-float gimb_y_angle_pid_param[7] = {2.5,0.1,0,0,0,0,700};
+gimbal_t gimbal = 
+{
+	.info = &gimbal_info,
+	.imu_update = Gimbal_Imu_Update,
+};
 
-float gimb_p_angle_in_pid_param[7] = {60,1,0,0,0,5000,28000};
+float gimb_y_angle_in_pid_param[7]   = {12,3.5,0,0,0,4000,28000};
 
-float gimb_p_angle_pid_param[7] = {3,0,0,0,0,0,500};
+float gimb_y_angle_pid_param[7]      = {2.5,0.1,0,0,0,0,700};
+
+float gimb_p_angle_in_pid_param[7]   = {60,1,0,0,0,5000,28000};
+
+float gimb_p_angle_pid_param[7]      = {3,0,0,0,0,0,500};
 
 int16_t can1_gimbal_send_buff[4];
 int16_t can2_gimbal_send_buff[4];
@@ -25,6 +33,9 @@ void Gimbal_Init_All(void)
 	Gim_Yaw.pid_init(&Gim_Yaw.pid.angle,gimb_y_angle_pid_param);
 	Gim_Pitch.pid_init(&Gim_Pitch.pid.angle_in,gimb_p_angle_in_pid_param);
 	Gim_Pitch.pid_init(&Gim_Pitch.pid.angle,gimb_p_angle_pid_param);
+	
+	gimbal.yaw_gimbal   = &Gim_Yaw;
+	gimbal.pitch_gimbal = &Gim_Pitch;
 }
 
 void Gimbal_Heartbeat(void)
@@ -33,28 +44,56 @@ void Gimbal_Heartbeat(void)
 	Gim_Yaw.heartbeat(&Gim_Yaw);
 }
 
-void Gimbal_Send(motor_t* gimbal,int16_t buff)
+void Gimbal_Send(motor_t* motor,int16_t *buff)
 {
-  if(gimbal->id.drive_type == M_CAN1)
+  if(motor->id.drive_type == M_CAN1)
 	{
-		CAN1_Send_With_int16_to_uint8(gimbal->id.tx_id,can1_gimbal_send_buff);
+		CAN1_Send_With_int16_to_uint8(motor->id.tx_id,can1_gimbal_send_buff);
 	}
-	if(gimbal->id.drive_type == M_CAN2)
+	if(motor->id.drive_type == M_CAN2)
 	{
-		CAN2_Send_With_int16_to_uint8(gimbal->id.tx_id,can2_gimbal_send_buff);
+		CAN2_Send_With_int16_to_uint8(motor->id.tx_id,can2_gimbal_send_buff);
 	}
+}
+
+void Gimbal_Imu_Update(gimbal_t* gimbal)
+{
+	gimbal->info->yaw_speed_measure = imu.base_info->yaw_dif_speed_ave;
+	
+	gimbal->info->pitch_speed_measure = imu.base_info->pitch_dif_speed_ave;
 }
 
 void Gimbal_Ctrl(void)
 {
+	gimbal.imu_update(&gimbal);
+	
+	
   
 }
 
 /*Part 1*/
-void Gimbal_Yaw_Mec_Ctrl(motor_t* gimbal)
+void Gimbal_Yaw_Mec_Ctrl(gimbal_t* gimbal)
 {
-	motor_rx_info_t *info = &gimbal->rx_info;
+	motor_rx_info_t *info = &gimbal->yaw_gimbal->rx_info;
 	
+	motor_t *motor = gimbal->yaw_gimbal;
 	
+	can1_gimbal_send_buff[motor->id.buff_p] = motor->c_pid2(&motor->pid.angle,&motor->pid.angle_in,
+	               info->angle,info->speed,gimbal->info->yaw_angle_target,1);
 	
+	Gimbal_Send(motor,can1_gimbal_send_buff);
 }
+
+void Gimbal_Pitch_Mec_Ctrl(gimbal_t* gimbal)
+{
+	motor_rx_info_t *info = &gimbal->pitch_gimbal->rx_info;
+	
+	motor_t *motor = gimbal->pitch_gimbal;
+	
+	can2_gimbal_send_buff[motor->id.buff_p] = motor->c_pid2(&motor->pid.angle,&motor->pid.angle_in,
+	               info->angle,info->speed,gimbal->info->yaw_angle_target,1);
+	
+	Gimbal_Send(motor,can2_gimbal_send_buff);
+}
+
+
--- HeroV1/MDK-ARM/USER/modules/gimbal.h
+++ HeroV1/MDK-ARM/USER/modules/gimbal.h
@@ -3,7 +3,36 @@
 
 #include "stm32f4xx_hal.h"
 #include "motor.h"
+#include "imu.h"
 
+typedef struct
+{
+	float yaw_angle_target;
+	
+	float yaw_speed_measure;
+	
+	float pitch_angle_target;
+	
+	float pitch_speed_measure;
+	
+}gimbal_info_t;
+
+typedef struct gimbal_class_t
+{
+	gimbal_info_t*  info;
+	
+	motor_t*  yaw_gimbal;
+	
+	motor_t*  pitch_gimbal;
+	
+	
+	void (*imu_update)(struct gimbal_class_t *gimbal);
+	void (*yaw_mec_ctrl)(struct gimbal_class_t *gimbal);
+	void (*pitch_mec_ctrl)(struct gimbal_class_t *gimbal);
+	
+}gimbal_t;
+
+void Gimbal_Imu_Update(gimbal_t* gimbal);
 void Gimbal_Init_All(void);
 void Gimbal_Heartbeat(void);
 void Gimbal_Ctrl(void);
